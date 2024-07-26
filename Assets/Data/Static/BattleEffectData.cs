128f9b973edd2219a9a72ce59f401ff6d2a81cb9
lian <lian1sususu@163.com>
2024-07-26 18:30:05 +0800

    add battle-effect-statement

--- Assets/Data/Static/BattleEffectData.cs
+++ Assets/Data/Static/BattleEffectData.cs
@@ -1,5 +1,5 @@
 using System;
-using UnityEngine;
+using Root;
 
 namespace Static
 {
@@ -29,10 +29,32 @@ namespace Static
         /// </summary>
         public Battler.BattlerType targetType;
 
-        public BattleEffect GetBattleEffect() =>
-            Root.ResourceManager.BattleEffect.GetItem(effectID);
+        public BattleEffect GetBattleEffect() => ResourceManager.BattleEffect.GetItem(effectID);
 
-        public DurationState GetDurationState() =>
-            Root.ResourceManager.DurationState.GetItem(effectID);
+        public DurationState GetDurationState() => ResourceManager.DurationState.GetItem(effectID);
+
+        public string GetStatement()
+        {
+            string text;
+            if (duration <= 0)
+            {
+                var be = GetBattleEffect();
+                if (be.type0 == BattleEffect.EffectType.Cost && be.type1 == 2)
+                    text = ResourceManager.ActorNormalItem.GetItem(effectValue).Name;
+                else if (be.type0 == BattleEffect.EffectType.RemoveState && be.type1 == 1)
+                    text = ResourceManager.DurationState.GetItem(effectValue).Name;
+                else if (be.type0 == BattleEffect.EffectType.RemoveState && be.type1 == 3)
+                    text = ResourceManager.Term.GetText((ElementType)effectValue);
+                else if (be.type0 == BattleEffect.EffectType.Special && be.type1 == 4)
+                    text = ResourceManager.Enemy.GetItem(effectValue).Name;
+                else
+                    text = effectValue.ToString();
+                return string.Format(be.Statement, text);
+            }
+            else
+            {
+                return string.Format(GetDurationState().Statement, effectValue.ToString());
+            }
+        }
     }
 }
